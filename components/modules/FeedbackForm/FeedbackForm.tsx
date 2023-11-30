9bdd330977e7b7e9f005be40e2a7cd322579dbeb
Andrey Emelianov <621077@mail.ru>
2023-11-30 17:51:02 +0300

    [feat(components)]: подключил emailJS, настроил аакаут на сервисе, и при отправке формы отправляется сообщение на почту с данными формы, проверил всё работает корректно

--- components/modules/FeedbackForm/FeedbackForm.tsx
+++ components/modules/FeedbackForm/FeedbackForm.tsx
@@ -1,5 +1,8 @@
 import { useStore } from 'effector-react'
 import { useForm } from 'react-hook-form'
+import { MutableRefObject, useRef, useState } from 'react'
+import emailjs from '@emailjs/browser'
+import { toast } from 'react-toastify'
 
 import { $mode } from '@/context/mode'
 import NameInput from './NameInput'
@@ -9,6 +12,7 @@ import MessageInput from './MessageInput'
 import { FeedbackInputs } from '@/types/feedbackForm'
 
 import styles from '@/styles/feedbackForm/index.module.scss'
+import spinnerStyles from '@/styles/spinner/index.module.scss'
 
 const FeedbackForm = () => {
   const {
@@ -17,11 +21,31 @@ const FeedbackForm = () => {
     formState: { errors },
   } = useForm<FeedbackInputs>()
 
+  const formRef = useRef() as MutableRefObject<HTMLFormElement>
+  const [spinner, setSpinner] = useState(false)
+
   const mode = useStore($mode)
   const darkModeClass = mode === 'dark' ? `${styles.dark_mode}` : ''
 
-  const submitForm = (data: FeedbackInputs) => {
-    console.log(data)
+  const submitForm = () => {
+    setSpinner(true)
+    emailjs
+      .sendForm(
+        'service_u1zyvlc',
+        'template_45kqwap',
+        formRef.current,
+        'pByf0zpPevbqkzTNS'
+      )
+      .then((result) => {
+        setSpinner(false)
+        toast.success(`Сообщение успешно отправленно! ${result.text}`)
+      })
+      .catch((error) => {
+        setSpinner(false)
+        toast.error(`Что-то пошло не так... ${error.text}`)
+      })
+
+    formRef.current.reset
   }
 
   return (
@@ -31,6 +55,7 @@ const FeedbackForm = () => {
       </h3>
 
       <form
+        ref={formRef}
         className={styles.feedback_form__form}
         onSubmit={handleSubmit(submitForm)}
       >
@@ -55,7 +80,16 @@ const FeedbackForm = () => {
           darkModeClass={darkModeClass}
         />
         <div className={styles.feedback_form__form__btn}>
-          <button>Отправить сообщение</button>
+          <button>
+            {spinner ? (
+              <span
+                className={spinnerStyles.spinner}
+                style={{ top: 6, left: '47%' }}
+              />
+            ) : (
+              'Отправить сообщение'
+            )}
+          </button>
         </div>
       </form>
     </div>
