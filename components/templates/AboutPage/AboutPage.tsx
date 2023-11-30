ef48f69e6599a8a5ffc114d1d2db84cb659bf4c6
Andrey Emelianov <621077@mail.ru>
2023-11-30 16:12:34 +0300

    [feat(components)]: реализовал страницу О компании. Реализовал компонент AboutPage с текстом о компании и изображениями

---
+++ components/templates/AboutPage/AboutPage.tsx
@@ -0,0 +1,45 @@
+/* eslint-disable @next/next/no-img-element */
+import { useStore } from 'effector-react'
+
+import { $mode } from '@/context/mode'
+
+import styles from '@/styles/about/index.module.scss'
+
+const AboutPage = () => {
+  const mode = useStore($mode)
+  const darkModeClass = mode === 'dark' ? `${styles.dark_mode}` : ''
+  return (
+    <section className={styles.about}>
+      <div className="container">
+        <h2 className={`${styles.about__title} ${darkModeClass}`}>
+          О Компании
+        </h2>
+
+        <div className={styles.about__inner}>
+          <div className={`${styles.about__info} ${darkModeClass}`}>
+            <p>
+              Компания &quot;BoilersHouse&quot; предлагает Вам запасные части
+              для европейских, корейских и отечественных газовых и электрических
+              котлов. 99% запчастей представленных на сайте постоянно
+              поддерживаются в наличии на нашем складе.
+            </p>
+            <p>
+              Ассортимент интернет-магазина &quot;BoilersHouse&quot; включает в
+              себя запасные части для котлов Arderia, Ariston, Baxi, Beretta,
+              Bosch, Buderus, Chaffoteaux, De Dietrich, Demrad, Electrolux,
+              Ferroli, Fondital, Immergas, Junkers, Koreastar, Nova Florida,
+              Saunier Duval, Sime, Tiberis, Vaillant, Viessmann, Westen.
+            </p>
+          </div>
+          <div className={`${styles.about__img} ${styles.about__img__top}`}>
+            <img src="/img/about-img.png" alt="изображение о компании 1" />
+          </div>
+          <div className={`${styles.about__img} ${styles.about__img__bottom}`}>
+            <img src="/img/about-img-2.png" alt="изображение о компании 2" />
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+export default AboutPage
